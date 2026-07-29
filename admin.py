7cf47c42820ce8c6c2d5f69723c1d6abdbd73d9c
Ambika Gupta <ambikagupta8899@gmail.com>
2026-07-29 14:10:29 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Event
-
-@admin.register(Event)
-class EventAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'event_type', 'page', 'timestamp')
-    list_filter = ('event_type',)
-    search_fields = ('user_id', 'page')
